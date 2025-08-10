3093d7d3e4e2f89cbfb54891db4437db9eb9476f
John Kenny <167453693+vibrantantelope@users.noreply.github.com>
2025-08-10 16:14:40 -0500

    feat: import leaflet via npm

--- family-history-map/src/App.jsx
+++ family-history-map/src/App.jsx
@@ -1,5 +1,10 @@
 import { useEffect } from 'react'
 import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import 'leaflet.markercluster/dist/MarkerCluster.css'
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
+import 'leaflet.markercluster'
+import 'leaflet-arrowheads'
 
 function App() {
   useEffect(() => {
